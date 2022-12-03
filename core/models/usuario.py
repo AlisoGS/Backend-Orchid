33e9e627ba7ae03ec63bcef333cc853113be9c9a
AlisoGS <alissonofficial12@gmail.com>
2022-12-03 00:18:42 -0300

    setuptools + requirements

--- core/models/usuario.py
+++ core/models/usuario.py
@@ -5,5 +5,6 @@ from django.db import models
 class Usuario(AbstractUser):
     data_nascimento = models.DateField(blank=True, null=True, default=None)
     foto = models.ImageField(upload_to="media/perfil", blank=True, null=True)
+
     def __str__(self):
         return self.username
