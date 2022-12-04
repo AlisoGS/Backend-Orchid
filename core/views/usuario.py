578e097719daf8062d8d7a106762083f2e46743a
AlisoGS <alissonofficial12@gmail.com>
2022-12-03 22:55:41 -0300

    liberando permissão para criação de usuarios

--- core/views/usuario.py
+++ core/views/usuario.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from core.models import Usuario
 from core.serializers import UsuarioDetailSerializer, UsuarioSerializer
@@ -13,3 +14,8 @@ class UsuarioViewSet(ModelViewSet):
     def get_queryset(self):
         usuario = self.request.user
         return Usuario.objects.filter(id=usuario.id)
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        return [IsAuthenticated()]
